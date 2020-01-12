657f2dabc8ddf1e871f21adf64c7b62b09b7699f
3mliu <emmy.liu.1998@gmail.com>
2020-01-12 11:58:55 -0500

    fix predict method

--- src/models/predict.py
+++ src/models/predict.py
@@ -8,17 +8,18 @@ from statistics import stdev
 from typing import List, Callable, Generic
 
 import numpy as np
-from sklearn import preprocessing, neighbors, metrics
+from scipy.spatial import distance
+from sklearn import preprocessing, metrics
 import statsmodels.stats.api as sms
 import pdb
 
 
-def get_attested_order(vecs_filename: str) -> List:
+def get_attested_order(vecs_filename: str, vecs_col: int = 1) -> List:
     rev_order = []
     with open(vecs_filename, "r") as vecs_file:
         reader = csv.reader(vecs_file)
         for row in reader:
-            rev_order.append(ast.literal_eval(row[1]))
+            rev_order.append(ast.literal_eval(row[vecs_col]))
     return rev_order[::-1]
 
 
@@ -37,6 +38,30 @@ def rank_on_1NN(emerged: List, unemerged: List) -> List:
             [item for item in closest.items()], key=lambda x: x[1], reverse=True
         )
     ]
+
+def make_rank_on_knn(k: int) -> Callable:
+    curr_iter = 0
+    def rank_on_knn(emerged: List, unemerged: List) -> List:
+        nonlocal curr_iter
+        curr_iter += 1
+        closest = {tuple(vec): 0 for vec in unemerged}
+        curr_k = min(k, curr_iter)
+        for candidate_vec in unemerged:
+            total_dist = 0
+            pdb.set_trace()
+            closest_vecs = emerged[distance.cosine(np.asarray(emerged), candidate_vec).argmin(curr_k)]
+            for vec in closest_vecs:
+                total_dist += distance.cosine(candidate_vec, vec)
+            closest[tuple(candidate_vec)] = total_dist
+
+        return [
+            list(item[0])
+            for item in sorted(
+                [item for item in closest.items()], key=lambda x: x[1], reverse=True
+            )
+        ]
+
+    return rank_on_knn
 
 
 # Rank on local is implemented by limiting the emerged list to points that emerged at the last timestep
@@ -98,15 +123,15 @@ def get_sim(vec_1: List, vec_2: List) -> float:
     )
 
 
-def get_emergence_order(emergence_filename: str) -> dict:
+def get_emergence_order(emergence_filename: str, vecs_col: int = 1) -> dict:
     emergence_order = {}
     with open(emergence_filename, "r") as order_file:
         reader = csv.reader(order_file)
         for row in reader:
             if int(row[0]) not in emergence_order:
-                emergence_order[int(row[0])] = [tuple(ast.literal_eval(row[1]))]
+                emergence_order[int(row[0])] = [tuple(ast.literal_eval(row[vecs_col]))]
             else:
-                emergence_order[int(row[0])].append(tuple(ast.literal_eval(row[1])))
+                emergence_order[int(row[0])].append(tuple(ast.literal_eval(row[vecs_col])))
 
     return emergence_order
 
@@ -144,7 +169,11 @@ def predict_seq(
             last_papers = prev_papers
 
         available_papers = all_vecs[:]
-        
+        for paper in emerged_papers:
+            available_papers.remove(
+                list(paper)
+            )  # Not using list comprehension so duplicates are preserved
+
         rank_err = 0
 
         for paper_vec in curr_papers:
@@ -152,6 +181,8 @@ def predict_seq(
                 predicted_order = ranking_model.rank_on_clusters(last_papers, available_papers)
             else:
                 predicted_order = ranking_func(last_papers, available_papers)
+            
+            #pdb.set_trace()
             prediction_error = error_func(predicted_order, next_papers)
             rank_err += prediction_error
            
@@ -165,22 +196,73 @@ def predict_seq(
         cumulative_err += rank_err
 
         prev_papers = curr_papers
-        for paper in emerged_papers:
-            available_papers.remove(
-                list(paper)
-            )  # Not using list comprehension so duplicates are preserved
-
+        
         if ranking_type == "CRP":
             ranking_model.update_clusters(prev_papers, len(prev_papers))
 
     return cumulative_err / len(all_vecs), rank_diff_per_timestep
 
 
-def get_rank_score_deprecated(
-    predicted: np.ndarray, actual: np.ndarray, next_index: int
-) -> int:
-    return predicted.index(actual)
 
+def predict_seq_multi(
+    all_vecs: List,
+    emergence_order: dict,
+    ranking_func_list: List,
+    error_func: Callable,
+    ranking_types: List,
+    ranking_model: Generic = None
+) -> tuple:
+    num_timesteps = max(emergence_order.keys())
+    emerged_papers = []
+    cumulative_err = {ranking_func: 0 for ranking_func in ranking_func_list}
+    rank_diff_per_timestep = {ranking_func: [] for ranking_func in ranking_func_list}
+
+    # Assume a single starting point in the space for now
+    for ranking_func, ranking_type in zip(ranking_func_list, ranking_types):
+        emerged_papers = []
+        for t in range(num_timesteps):
+            prev_papers = None
+            curr_papers = emergence_order[t]
+            next_papers = emergence_order[t + 1]
+            emerged_papers.extend(curr_papers)
+
+            last_papers = emerged_papers
+            if (
+                prev_papers and ranking_type == "local"
+            ):  # only factor in the previous timestep for local algos
+                last_papers = prev_papers
+
+            available_papers = all_vecs[:]
+            
+            rank_err = 0
+
+            for paper_vec in curr_papers:
+                if ranking_type == "CRP":
+                    predicted_order = ranking_model.rank_on_clusters(last_papers, available_papers)
+                else:
+                    predicted_order = ranking_func(last_papers, available_papers)
+                prediction_error = error_func(predicted_order, next_papers)
+                rank_err += prediction_error
+            
+                random_order = get_random(last_papers, available_papers)
+                random_rank_err = error_func(random_order, next_papers)
+                rank_diff_per_timestep[ranking_func].append(random_rank_err - prediction_error)
+
+            rank_err = rank_err / len(
+                curr_papers
+            )  # Just take the average rank error at timestep?
+            cumulative_err[ranking_func] += rank_err
+
+            prev_papers = curr_papers
+            for paper in emerged_papers:
+                available_papers.remove(
+                    list(paper)
+                )  # Not using list comprehension so duplicates are preserved
+
+            if ranking_type == "CRP":
+                ranking_model.update_clusters(prev_papers, len(prev_papers))
+
+    return cumulative_err / len(all_vecs), rank_diff_per_timestep
 
 def get_rank_score_avg(predicted: np.ndarray, actual: List) -> int:
     rank_diff = 0
@@ -206,11 +288,12 @@ def shuffle_test(n_iter: int, target_val: int, emergence_order: dict, vecs_filen
     lower = 0
     cumulative_rank_diffs = []
     trial_timestep_rank_diff = None
+    _attested_order = get_attested_order(vecs_filename)
+    _emergence_order = get_emergence_order(order_filename)
 
     for i in range(n_iter):
-        print(i)
-        attested_order = get_attested_order(vecs_filename)
-        emergence_order = get_emergence_order(order_filename)
+        attested_order = _attested_order
+        emergence_order = _emergence_order
 
         random.seed()
         rand_val, rank_diff_at_timestep = predict_seq(
@@ -224,8 +307,9 @@ def shuffle_test(n_iter: int, target_val: int, emergence_order: dict, vecs_filen
         else:
             trial_timestep_rank_diff = [sum(x) for x in zip(trial_timestep_rank_diff, rank_diff_at_timestep)]
 
+        print("random error: ", rand_val)
         if rand_val > target_val: # I reversed the p value by mistake. The p value returned is actually 1 - p
-            higher += 1
+            higher += 1 
         else:
             lower += 1
 
@@ -248,19 +332,25 @@ def shuffle_test(n_iter: int, target_val: int, emergence_order: dict, vecs_filen
 
 
 if __name__ == "__main__":
-    all_vecs = get_attested_order("data/hinton_paper_vectors.csv")
-    emergence_order = get_emergence_order("data/ordered_by_date.csv")
+    vecs_filename = "data/turing_winners/vecs-abstracts-only/Judea-Pearl.csv"
+    order_filename = "data/turing_winners/vecs-abstracts-ordered/Judea-Pearl.csv"
+
+    #vecs_filename = "data/turing_winners/abstracts/geoff/Geoff-Hinton-title-vecs.csv"
+    #order_filename = "data/turing_winners/abstracts/geoff/Geoff-Hinton-title-ordered.csv"
+    all_vecs = get_attested_order(vecs_filename)
+    emergence_order = get_emergence_order(order_filename)
     rank_on_prog = rank_on_progenitor(all_vecs[0])
     # all_vecs = get_attested_order("../../data/testing_rank.csv")
     # emergence_order = get_emergence_order("../../data/testing_rank.csv")
 
-    """error, _ = predict_seq(
+    error, rank_diff_per_timestep = predict_seq(
         all_vecs,
         emergence_order,
-        rank_on_1NN,
-        get_rank_score_avg,
+        rank_on_prog,
+        get_rank_score_best,
         ranking_type="global",
-    )"""
-    
-    error = 5
-    print(shuffle_test(n_iter=100, target_val=error, emergence_order=emergence_order))
+    )
+    print("error: ", error)
+    print("rank diff per timestep: ", rank_diff_per_timestep)
+
+    print(shuffle_test(n_iter=100, target_val=error, emergence_order=emergence_order, vecs_filename=vecs_filename, order_filename=order_filename))
